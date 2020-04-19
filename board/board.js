26d11a5b1af2e9715ca6d59fb71ef68c47aca11e
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2020-04-19 20:19:15 +0900

    update

--- board/board.js
+++ board/board.js
@@ -1,25 +1,75 @@
-class Checker {
-    constructor(id, w, h, x, y, image) {
+class BackgammonObj {
+    constructor(id, x, y) {
         this.id = id;
-        this.w = w;
-        this.h = w;
         this.x = x;
         this.y = y;
-        this.image = image;
-        this.moving = false;
 
         this.el = document.getElementById(this.id);
-        this.el.hidden = true;
-        this.el.draggable = true;
+
+        this.w = this.el.firstChild.width;
+        this.h = this.el.firstChild.height;
+
+        this.el.style.position = "absolute";
+        this.el.style.cursor = "move";
+        this.el.style.userSelect = "none";
+
+        this.el.hidden = false;
+        this.el.draggable = false;
+    }
+}
+
+class Board extends BackgammonObj {
+    constructor(id, x, y) {
+        super(id, x, y);
+
+        this.area_w = 324;
+        this.aria_h = 684;
+
+        this.point_w = this.area_w / 6;
+        this.point_h = this.area_h / 2;
+
+        this.x[0] = 108;
+        this.y[0] = 27;
+        this.x[1] = 540;
+        this.y[1] = this.y[0];
+
+        for (let i = 1; i < 6; i++) {
+            
+        }
     }
 
-    on(x, y) {
+    static xy2point(x, y) {
+        point = undefined;
+        
+        return point;
+    }
+}
+
+class Checker extends BackgammonObj {
+    constructor(id, x, y) {
+        super(id, x, y);
+
+        this.el.hidden = true;
+
+        this.el.onmousedown = this.on_mouse_down.bind(this);
+        this.el.onmouseup = this.on_mouse_up.bind(this);
+        this.el.onmousemove = this.on_mouse_move.bind(this);
+        this.el.ondragstart = this.on_drag_start.bind(this);
+
+        this.moving = false;
+
+        this.move(this.x,this.y);
+    }
+
+    on() {
+        console.log('on');
         this.el.hidden = false;
-        this.x = x;
-        this.y = y;
+
+        this.move(this.x, this.y);
     }
 
     off() {
+        console.log('off');
         this.el.hidden = true;
     }
     
@@ -27,7 +77,6 @@ class Checker {
         console.log("move(" + x + "," + y +")");
         this.x = x;
         this.y = y;
-
         this.el.style.left = (this.x - this.w / 2) + "px";
         this.el.style.top = (this.y - this.h / 2) + "px";
     }
@@ -35,18 +84,21 @@ class Checker {
     on_mouse_down(e) {
         console.log("on_mouse_down");
         this.moving = true;
-        // this.move(e.clientX, e.clientY);
+        console.log("zIndex=" + this.el.style.zIndex);
+        this.el.style.zIndex = 1000;
     }
 
     on_mouse_up(e) {
         console.log("on_mouse_up");
         this.moving = false;
+        this.el.style.zIndex = 0;
+        this.off();
     }
 
     on_mouse_move(e) {
         console.log("on_mouse_move:" + this.moving);
-        // if (this.moving) {
-        if (e.buttons> 0) {
+        if (this.moving) {
+        //if (e.buttons> 0) {
             this.move(e.clientX, e.clientY);
         }
     }
@@ -63,38 +115,18 @@ window.onload = function () {
     let move_start_x = 0;
     let move_start_y = 0;
 
-    let c1 = new Checker("c1", 100, 100, 100, 100, "images/checkerA.png");
-    let c2 = new Checker("c2", 200, 100, 100, 100, "images/checkerB.png");
-
-    let el_c1 = document.getElementById("c1");
-    el_c1.onmousedown = function(e) {
-        c1.on_mouse_down(e);
-    };
-    el_c1.onmouseup = function(e) {
-        c1.on_mouse_up(e);
-    };
-    el_c1.onmousemove = function(e) {
-        c1.on_mouse_move(e);
-    };
+    let board = new Board("board", 0, 0);
+    let c1 = new Checker("c1", 100, 100);
+    let c2 = new Checker("c2", 200, 100);
 
     let el_t = document.getElementById("t");
+
     el_t.onmousedown = function(e) {
         move_flg = true;
         move_start_x = e.clientX - parseInt(document.getElementById("t").style.left.replace("px",""));
         move_start_y = e.clientY - parseInt(document.getElementById("t").style.top.replace("px",""));
 
-        c1.on(e.clientX, e.clientY);
-    };
-
-    el_t.onmouseup = function(e) {
-        move_flg = false;
-    };
-
-    el_t.onmousemove = function(e) {
-        if(move_flg) {
-            console.log("a");
-            document.getElementById("t").style.left = (e.clientX - move_start_x) + "px";
-            document.getElementById("t").style.top = (e.clientY - move_start_y) + "px";    
-        }
+        c1.on();
+        c2.on();
     };
 };
