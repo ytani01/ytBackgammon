471217fd7df7e0b8ed2e18b58b689816c1ee6600
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2021-06-06 02:25:55 +0900

    FIX: requirements.txt and more..

--- ytbg-boot.sh
+++ ytbg-boot.sh
@@ -3,8 +3,10 @@
 MYNAME=`basename $0`
 images="images2 images0a images1a images3"
 
+VENVDIR="$HOME/env0-ytbg"
+
 for i in 1 2 3 4; do
     _port=`expr 5000 + $i`
     _images=`echo $images | cut -d ' ' -f $i`
-    ytbg.sh /home/localhost/ytani/env1-backgammon -d -p $_port -i $_images $i &
+    ytbg.sh $VENVDIR -d -p $_port -i $_images $i &
 done
